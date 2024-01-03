d8ac34f077dcce7e2d7d0501830f747af82d2dcb
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-01-04 00:05:02 +0530

    All Clear

--- src/Main.java
+++ src/Main.java
@@ -73,23 +73,23 @@
 //    }
 //}
 
-import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        boolean isPrime = true;
-        for (int i = 2; i >= number; i++){
-            if(number % i == 0){
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime){
-            System.out.println("Number is non Prime");
-        }
-        else {
-            System.out.println("Number is Prime !!");
-        }
-    }
-}
+//import java.util.*;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int number = sc.nextInt();
+//        boolean isPrime = true;
+//        for (int i = 2; i >= number; i++){
+//            if(number % i == 0){
+//                isPrime = false;
+//                break;
+//            }
+//        }
+//        if (isPrime){
+//            System.out.println("Number is non Prime");
+//        }
+//        else {
+//            System.out.println("Number is Prime !!");
+//        }
+//    }
+//}
