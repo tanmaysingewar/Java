6de358cafc01f4b632b829c15f5c50bd06226251
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-01-03 21:38:45 +0530

    While Loop Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-import java.util.*;
+//import java.util.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int age = sc.nextInt();
+//
+//        if(age > 18){
+//            System.out.println("You are Adult");
+//        }
+//        else {
+//            System.out.println("You are not Adult");
+//        }
+//    }
+//}
 
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-
-        if(age > 18){
-            System.out.println("You are Adult");
-        }
-        else {
-            System.out.println("You are not Adult");
+        int input = sc.nextInt();
+        int initialValur = 0;
+        int count = 0;
+        while(initialValur <= input){
+            count = count + initialValur;
+            System.out.println("Current No: "+initialValur+" Count: " + count);
+            initialValur++;
         }
     }
 }
