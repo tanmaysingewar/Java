a66bc29bdf81f79ff36b9c157ed3b64ad1c32b7d
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-01-04 00:03:06 +0530

    Space removed

--- src/Main.java
+++ src/Main.java
@@ -78,16 +78,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-
         boolean isPrime = true;
-
         for (int i = 2; i >= number; i++){
             if(number % i == 0){
                 isPrime = false;
                 break;
             }
         }
-
         if (isPrime){
             System.out.println("Number is non Prime");
         }
