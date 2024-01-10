92b4e59ebdb023c9bc739a5867ed3ddbd5a2747d
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-01-11 00:05:34 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -95,14 +95,3 @@
 //}
 
 
-//import java.util.*;
-//public class Main {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int number = sc.nextInt();
-//        if(number == 5){
-//            System.out.println("Number is "+number);
-//          }
-//  
-//    }
-//}
