c0cbdccbc259e22ff8c1c99f0e6ca5f92f8f4619
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-01-16 23:19:44 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -60,6 +60,9 @@
 
 //import java.util.*;
 //public class Main {
+// public int add(int num1, int num2) {
+//    return num1 + num2;
+// }
 //    public static void main(String[] args) {
 //        Scanner sc = new Scanner(System.in);
 //        int number = sc.nextInt();
