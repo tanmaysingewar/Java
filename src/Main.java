e52df85d9a6752093a30c3d0dc033aa10bf9402b
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-01-17 20:24:48 +0530

    inverted and rotated pyramid

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,21 @@ public class Main{
             System.out.println();
         }
     }
+
+    public static void inv_rot_pyramid(int row,int col){
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if(col - i >= j){
+                    System.out.print(" ");
+                }else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        hollow_rectangle(4,4);
+//        hollow_rectangle(4,4);
+        inv_rot_pyramid(4,4);
     }
 }
