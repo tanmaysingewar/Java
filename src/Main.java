55c0e161f66054b19376c380d52976d7d8a777e7
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-01-17 20:57:24 +0530

    01 triangle & floyd triangle

--- src/Main.java
+++ src/Main.java
@@ -35,9 +35,36 @@ public class Main{
             System.out.println("");
         }
     }
+
+    public static void floyd_triangle(int num){
+        int counter = 1;
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void static_triangle(int num){
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i ; j++) {
+                if((i+j) % 2  == 0){
+                    System.out.print("1");
+                }else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
     public static void main(String[] args) {
 //        hollow_rectangle(4,4);
 //        inv_rot_pyramid(4,4);
-        inv_half_pyramid(4,4);
+//        inv_half_pyramid(4,4);
+        floyd_triangle(5);
+//        static_triangle(5);
     }
+
 }
