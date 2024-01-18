292654ab867d3fef6058e894543b86a5bee891d3
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-01-18 22:40:28 +0530

    reversing Array

--- src/Main.java
+++ src/Main.java
@@ -166,11 +166,30 @@ public class Main{
         return -1;
     }
 
+    public static void reverse(int[] array){
+        int first = 0, last = array.length - 1;
+
+        while (first < last) {
+            int temp = array[last];
+            array[last] = array[first];
+            array[first] = temp;
+
+            first++;
+            last--;
+        }
+    }
+
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7};
+        int[] array = {1,2,3,4,5,6};
 //        int res = linearSearch(array,4);
 //        int res = maxInArray(array);
 //        int res = binarySearch(array, 3);
 //        System.out.println(res);
+
+        reverse(array);
+
+        for (int i = 0; i < array.length ; i++) {
+            System.out.print(array[i]);
+        }
     }
 }
