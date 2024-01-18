78c64a5147d7c97c6966db68ffc506e0b7a634ba
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-01-18 19:19:44 +0530

    Binary Search

--- src/Main.java
+++ src/Main.java
@@ -145,6 +145,27 @@ public class Main{
         return maxNum;
     }
 
+    public static int binarySearch(int[] array, int key){
+        int start = 0;
+        int end = array.length - 1;
+
+
+        while (start <= end){
+            int mid = (start + end) / 2;
+
+            if(array[mid] == key){
+                return mid;
+            }
+            if (array[mid] < key){
+                start = mid + 1;
+            }
+            else {
+                end = mid -1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] array = {1,2,3,4,5,6,7};
 //        int res = linearSearch(array,4);
