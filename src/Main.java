924dcacf91f382c20faab636d534f47e16d63b7f
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-01-23 19:47:52 +0530

    Selection Sort

--- src/Main.java
+++ src/Main.java
@@ -303,6 +303,19 @@ public class Main{
         }
     }
 
+    public static void  SelectionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[minPos] > arr[j]){
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
 
 
     public static void printArr(int[] arr){
