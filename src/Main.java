360764286da5a65e1e036578daadab94b2f3d995
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-01-28 14:09:23 +0530

    get Shortest Path

--- src/Main.java
+++ src/Main.java
@@ -389,9 +389,29 @@ public class Main{
         return true;
     }
 
+    public static float getShortestPath(String path){
+        int x = 0,y=0;
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            if(dir == 'S'){
+                y--;
+            }else if (dir =='N'){
+                y++;
+            } else if (dir == 'W') {
+                x--;
+            }else {
+                x++;
+            }
+        }
+        int X2 = x*x;
+        int Y2 = y*y;
+
+        return (float) Math.sqrt(X2+Y2);
+    }
 
     public static void main(String[] args) {
         String str = "roorm";
-        System.out.println(isPalindrome(str));
+//        System.out.println(isPalindrome(str));
+        System.out.println(getShortestPath("WESWSNSNWN"));
     }
 }
